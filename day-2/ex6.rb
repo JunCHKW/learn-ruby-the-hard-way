a3c67487f3ee241ca8daf87d604cd8a0dbb27950
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-18 12:50:16 +0700

    day2, ex6 strings and text

---
+++ day-2/ex6.rb
@@ -0,0 +1,25 @@
+# variable with value, the name is types_of_people and the value is 10
+types_of_people = 10
+# call variable in string using #{variable}
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# boolean variable
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print out variable using "puts"
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
