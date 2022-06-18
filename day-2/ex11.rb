56e12326fb52dadeaa4f61e890e89b1e07660ea1
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-18 16:33:14 +0700

    day2, ex11 asking question(input)

---
+++ day-2/ex11.rb
@@ -0,0 +1,13 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weight? "
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+print "And then what is your hobby? "
+hobby = gets.chomp
+
+puts "So, your hobby is #{hobby}, I like it too, nice to know you."
