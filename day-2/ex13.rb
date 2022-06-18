cf08565c32a1f57e5d5ac24fd14d4d29a610ec1d
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-18 17:19:08 +0700

    day2, modify and add some notes on ex13

--- day-2/ex13.rb
+++ day-2/ex13.rb
@@ -4,3 +4,9 @@ first, second, third = ARGV
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+
+# should using $stdin.gets.chomp if want to use both of gets.chomp and ARGV
+print "\nInput a number: "
+number = $stdin.gets.chomp.to_i
+
+puts "Your number is #{number}"
