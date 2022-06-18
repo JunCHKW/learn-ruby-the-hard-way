55f3e0ee8a011ca7dd91eb3dd8759b74156405b6
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-18 15:03:17 +0700

    day2, ex9 printing, printing, printing

---
+++ day-2/ex9.rb
@@ -0,0 +1,15 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here the days: #{days}"
+puts "Here the months: #{months}"
+
+# puts %q{...} => Printing paragraph text/quote
+puts %q{
+There's something going on here.
+    With this weird quote
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+}
