2e75adef66cb0bd4432d4c7b0817b2f59d4b9e66
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-18 17:08:41 +0700

    day2, ex13 parameters, unpacking, variables

---
+++ day-2/ex13.rb
@@ -0,0 +1,6 @@
+# run with "ruby ex13.rb first_argument second_argument third_argument"
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
