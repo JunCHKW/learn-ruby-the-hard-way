885a1d4bf46e28d8ae797d0b9c96fc56c4d7dc3b
JunCHKW <juniarto.christiawan@koinworks.com>
2022-06-21 16:24:31 +0700

    day5, ex34 accessing elements of arrays

---
+++ day-5/ex34.rb
@@ -0,0 +1,33 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+# => the animal at [1] is the second animal and is ruby
+puts animals[1]
+
+# The third (3rd) animal.
+# => the third animal is at [2] and is peacock
+puts animals[2]
+
+# The first (1st) animal.
+# => the first animal is at [0] and is bear
+puts animals[0]
+
+# The animal at 3.
+# => the animal at [3] is the fourth animal and is kangaroo
+puts animals[3]
+
+# The fifth (5th) animal.
+# => the fifth animal is at [4] and is whale
+puts animals[4]
+
+# The animal at 2.
+# => the animal at [2] is the third animal and is peacock
+puts animals[2]
+
+# The sixth (6th) animal.
+# => the sixth animal is at [5] and is platypus
+puts animals[5]
+
+# The animal at 4.
+# => the animal at [4] is the fifth animal and is whale
+puts animals[4]
